b6ea59f580e4597f51e414ee91e011bef24dc7b0
frankbryce <jonnyjack7@gmail.com>
2022-02-02 22:01:23 -0500

    Add basic turtle programs based off of numberphile video today.
    "Plotting Pi and Searching for Mona Lisa"

--- project/static/client.js
+++ project/static/client.js
@@ -15,4 +15,54 @@ socket.on('json', function(data) {
 function random(w, h) {
     socket.emit('random', w, h);
 }
-random(10, 10);
+
+base = 3n
+ang = Math.PI/(Number(base)/2)  // base 10
+ndigits = 100000n
+
+let x = 3n * (base ** (ndigits+20n));
+let pi = x;
+{
+	let i = 1n;
+	while (x > 0) {
+	        x = x * i / ((i + 1n) * 4n);
+        	pi += x / (i + 2n);
+	        i += 2n;
+	}
+}
+pi = pi.toString(Number(base));
+
+points=[[0,0]]
+curr = [0,0];
+theta = 0;
+minX = 0;
+minY = 0;
+maxX = 0;
+maxY = 0;
+for (var i=0;i<ndigits;i++) {
+	digit = pi[i];
+	theta += ang*digit;
+	curr[0] += 50*Math.cos(theta);
+	curr[1] += 50*Math.sin(theta);
+	points.push([curr[0], curr[1]])
+	minX = Math.min(curr[0], minX);
+	minY = Math.min(curr[1], minY);
+	maxX = Math.max(curr[0], maxX);
+	maxY = Math.max(curr[1], maxY);
+}
+
+
+function draw(context) {
+	context.moveTo(points[0][0], points[0][1]);
+	for (var i=1;i<points.length;i++) {
+		context.lineTo(points[i][0], points[i][1]);
+	}
+	return context;
+}
+
+d3.select("#field")
+	.attr("viewBox", "" + minX + " " + minY + " " + (maxX-minX) + " " + (maxY-minY))
+	.append("path")
+		.style("stroke", "black")
+		.style("fill", "none")
+		.attr("d", draw(d3.path()));
